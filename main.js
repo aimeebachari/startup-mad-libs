413b729799df53c092c04f58dd646787aa2a058c
Aimee Bachari <aimeebachari@gmail.com>
2018-07-16 20:07:50 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let companyName = 'Aimee\'s Really Cool Tech Company';
+let anOffering = 'a new social app';
+let aTargetAudience = 'kids';
+let solveAProblem = 'learn to code';
+let secretSauce = 'by building some cool stuff';
+
+let pitch = `My company, ${companyName}\nis developing ${anOffering}\nto help ${aTargetAudience}\n${solveAProblem}\n${secretSauce}`;
+
+console.log(pitch);
